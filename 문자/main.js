936b2117db90c32a64c41a9696ba195852eaed5b
yongwan98 <yongwan1998@gmail.com>
2022-04-25 11:41:41 +0900

    문자 정리

--- 문자/main.js
+++ 문자/main.js
@@ -7,3 +7,30 @@
 // null
 // Array: []
 // Object: {}
+
+// const result = 'Hello world'.indexOf('world') // === String.prototype.indexOf()
+// console.log(result)
+
+// length
+const str0 = '0123'
+console.log('length : ', str0.length)
+
+// indexOf
+const str1 = 'Hello world!'
+console.log('indexOf : ', str1.indexOf('world'))
+
+// slice
+const str2 = 'Hello world!'
+console.log('slice : ', str2.slice(0, 3))
+
+// replace
+const str3 = 'Hello world!'
+console.log('replace : ', str3.replace('world', 'Heropy'))
+
+// match
+const str4 = 'wyw1998@naver.com'
+console.log('match : ', str4.match(/.+(?=@)/)[0])
+
+// trim
+const str5 = '    Hello world!     '
+console.log('trim : ', str5.trim())
