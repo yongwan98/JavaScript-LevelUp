39ba73e49a0dcc025044eac03be1a85592bb3772
yongwan98 <yongwan1998@gmail.com>
2022-04-26 14:04:10 +0900

    불변성 정리 중

--- 불변성/main.js
+++ 불변성/main.js
@@ -1,3 +1,4 @@
+// 원시형 데이터
 let a = 1
 let b = 4
 console.log(a, b, a === b)
@@ -7,3 +8,15 @@ a = 7
 console.log(a, b, a === b)
 let c = 1
 console.log(b, c, b === c)
+
+// 참조형 데이터
+let a1 = { K: 1 }
+let b1 = { K: 1 }
+console.log(a1, b1, a1 === b1)
+a1.k = 7
+b1 = a1
+console.log(a1, b1, a1 === b1)
+a1.k = 2
+console.log(a1, b1, a1 === b1)
+let c1 = b1
+console.log(a1, b1, c1, a1 === c1)
