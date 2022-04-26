c0d4c3a994b95b6cd5dfc6bbfa9a45fda46a87e4
yongwan98 <yongwan1998@gmail.com>
2022-04-26 09:25:30 +0900

    배열(2) 정리

--- 배열(2)/main.js
+++ 배열(2)/main.js
@@ -1,22 +1,22 @@
-// .filter()
 const numbers = [1, 2, 3, 4]
 const fruits = ['Apple', 'Banana', 'Cherry']
 
+// .filter()
 // const a = numbers.map(number => {
 //   return number < 3
 // })
 // 화살표 함수 간소화
 const a = numbers.map(number => number < 3)
-console.log(a)
+console.log('.map(): ', a)
 
 // const b = numbers.filter(number => {
 //   return number < 3
 // })
 // 화살표 함수 간소화
 const b = numbers.filter(number => number < 3)
-console.log(b)
+console.log('.filter(): ', b)
 
-// .finde()
+// .find()
 // const a1 = fruits.find(fruit => {
 //   return /^B/.test(fruit)
 // })
@@ -33,3 +33,32 @@ console.log('.find(): ', a1)
 // 화살표 함수 간소화
 const b1 = fruits.findIndex(fruit => /^B/.test(fruit))
 console.log('.findIndex(): ', b1)
+
+// .includes()
+const a2 = numbers.includes(3)
+console.log('.includes(): ', a2)
+
+const b2 = fruits.includes('Heropy')
+console.log('.includes(): ', b2)
+
+// .push()
+// 원본 수정됨!
+// numbers.push(5)
+// console.log('.push(): ', numbers)
+
+// .unshift()
+// 원본 수정됨!
+// numbers.unshift(0)
+// console.log('.unshift(): ', numbers)
+
+// .reverse()
+// 원본 수정됨!
+// numbers.reverse()
+// fruits.reverse()
+// console.log('.reverse(): ', numbers)
+// console.log('.reverse(): ', fruits)
+
+// .splice()
+// 원본 수정됨!
+// numbers.splice(0, 1)
+// console.log('.splice(): ', numbers)
