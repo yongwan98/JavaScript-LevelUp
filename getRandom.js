9b70082b1432b17458f74e62379e825ab05165f5
yongwan98 <yongwan1998@gmail.com>
2022-04-28 11:12:45 +0900

    가져오기, 내보내기 정리

--- getRandom.js
+++ getRandom.js
@@ -1,6 +1,14 @@
-export default function random() {
+export function random() {
   // floor을 통해 내림 처리를 한다.
   // random을 통해 난수가 생성된다.
   // 난수는 소수점값만 나오기 때문에 * 10을 통해 정수도 값이 나올 수 있게 한다.
   return Math.floor(Math.random() * 10)
 }
+
+export let information = {
+  name: 'Heropy',
+  age: 85,
+  email: 'wyw1998@naver.com'
+}
+
+export default 123
--- 가져오기, 내보내기/main.js
+++ 가져오기, 내보내기/main.js
@@ -1,7 +1,10 @@
 import _ from 'lodash' // From `node_modules`!
 import getType from '../getType' // getType.js
-import getRandom from '../getRandom' // getRandom.js
+// import { random, information as info } from '../getRandom' // getRandom.js
+import * as R from '../getRandom' // getRandom.js
 
 console.log(_.camelCase('the hello world'))
 console.log(getType([1, 2, 3]))
-console.log(getRandom(), getRandom())
+// console.log(random(), random())
+console.log(R.random())
+console.log(R)
