129c14ee420c773db477378c972f4c5790de17ac
yongwan98 <yongwan1998@gmail.com>
2022-04-28 13:11:31 +0900

    JSON 정리

---
+++ JSON/main.js
@@ -0,0 +1,21 @@
+// JSON (JavaScript Object Notation)
+// 자바스크립트의 객체 표기법
+import myData from './myData.json'
+console.log(myData)
+
+const user = {
+  name: 'HEROPY',
+  age: 85,
+  emails: [
+    'thesecon@gmail.com',
+    'neo@zillinks.com'
+  ]
+}
+console.log('user: ', user)
+
+const str = JSON.stringify(user)
+console.log('str: ', str)
+console.log(typeof str)
+
+const obj = JSON.parse(str)
+console.log('obj: ', obj)
